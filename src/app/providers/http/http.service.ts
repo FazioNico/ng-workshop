38ffc8c1fba736ab3330b348be8fa91a0e42c8f6
FazioNico <contact@nicolasfazio.ch>
2018-11-26 11:33:20 +0100

    add inmemory

--- src/app/providers/http/http.service.ts
+++ src/app/providers/http/http.service.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
 
 @Injectable()
 export class HttpService {
+
+  public datas: any;
 
   constructor(
     private _http: HttpClient
   ) { }
 
   get(url): Observable<any> {
-    return this._http.get(url)
+    if (this.datas) return of(this.datas);
+    // only do it if don have `datas` proprets defind.
+    return this._http.get(url).pipe(
+      tap(response => this.datas = response)
+    )
   }
 }
